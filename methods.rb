2c23e0feb59b981c7e6571f5d67cce8a894212c6
alexandra <alexandraschurman@gmail.com>
2022-02-21 10:07:05 -0800

    commit tree cd224ead7fdcb788dc36c3244a8216acd0e8b467

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+greet_with_default("Naureen")
+
+def add (num1, num2)
+    return num1 + num2
+end
+add(2, 5)
+
+def halve (num)
+     num/2
+    if num class = string
+         return nil
+     end
+ halve(6)
